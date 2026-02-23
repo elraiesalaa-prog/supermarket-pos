0e625f0ea113c058f233d511b4006738abe6e742
elraiesalaa-prog <elraiesalaa@gmail.com>
2026-02-23 11:08:10 +0200

    Refactor supabase client initialization
    Refactor supabase client initialization to assign to window.supabaseClient.

--- supabase.js
+++ supabase.js
@@ -1,4 +1,7 @@
 const supabaseUrl = "https://vmxbedovoahuggfpcmyk.supabase.co";
 const supabaseKey = "sb_publishable_y-bZryAIeagBLR0Kg0mw2Q_YwpmadhU";
 
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+window.supabaseClient = window.supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
