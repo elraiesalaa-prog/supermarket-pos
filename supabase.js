8674cc44c7835f7b4d24293583b453f587dda88a
elraiesalaa-prog <elraiesalaa@gmail.com>
2026-02-23 10:58:23 +0200

    Update Supabase client initialization with new URL and key

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const supabase = window.supabase.createClient(
-"YOUR_SUPABASE_URL",
-"YOUR_PUBLIC_ANON_KEY"
-);
+const supabaseUrl = "https://vmxbedovoahuggfpcmyk.supabase.co";
+const supabaseKey = "sb_publishable_y-bZryAIeagBLR0Kg0mw2Q_YwpmadhU";
+
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
